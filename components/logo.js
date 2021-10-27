3bc581476c967339db46a1e7d7aff0579e1a5b81
SeanChen0298 <yoonsean0298@gmail.com>
2021-10-27 13:07:45 +0800

    Change logo to gear

--- components/logo.js
+++ components/logo.js
@@ -22,20 +22,20 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-  const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+  const gearImg = `/images/gear${useColorModeValue('', '-dark')}.png`
 
   return (
     <Link href="/">
       <a>
         <LogoBox>
-          <Image src={footPrintImg} width={20} height={20} alt="logo" />
+          <Image src={gearImg} width={20} height={20} alt="logo" />
           <Text
             color={useColorModeValue('gray.800', 'whiteAlpha.900')}
             fontFamily='M PLUS Rounded 1c", sans-serif'
             fontWeight="bold"
             ml={3}
           >
-            Sean Chen
+            &nbsp; Sean Chen
           </Text>
         </LogoBox>
       </a>
